2c0601c1bceac2982465d77bc519c7bb48c570a9
Makita7 <sophiamakita@gmail.com>
2022-09-30 11:17:57 -0300

    sectionOne fix

--- sunnyside/src/components/SectionOne.vue
+++ sunnyside/src/components/SectionOne.vue
@@ -7,7 +7,7 @@
                 <p>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
                 <a href="">
                     Learn more
-                    <div class="pink"></div>
+                    <div class="yellow"></div>
                 </a>
             </div>
             <div class="container">
@@ -61,8 +61,8 @@
         color: var(--VeryDarkDesaturatedBlue);
         text-decoration: none;
     }
-    .yellow{
-        text-decoration-color: var(--Yellow) !important;
+    .yellow {
+        background-color: var(--Yellow) !important;
         height: 1rem;
         width: 25%;
         border-radius: 6px;
